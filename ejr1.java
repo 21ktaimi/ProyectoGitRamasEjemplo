7f86462676c4fcbc8724a3fd34774d5c7b2da886
21ktaimi <105429167+21ktaimi@users.noreply.github.com>
2022-11-22 21:15:43 +0100

    Update ejr1.java

--- ejr1.java
+++ ejr1.java
@@ -1,7 +1,6 @@
 public class ejr1 {
-    public static void main (String[] args) {
-    
-        System.out.println("Hola mundo");
-        
-    }
+    public static void imprimeSaludo() { 
+System.out.println(“Hei”);	
+}
+
 }
